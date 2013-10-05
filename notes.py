94bf14e8465e26a7c0bd876e25dea557f260fc15
ejmahler <join.together@gmail.com>
2013-10-05 15:21:46 -0500

    added logic to compute which notes are sharped or flatted for any key

--- notes.py
+++ notes.py
@@ -33,25 +33,41 @@ def create_note_map():
 def get_key(key_name, key_type):
     
     #set up the major key indexes
-    note_index_map = {'major':{}}
+    note_index_map = {}
     
     for i, name_list in enumerate(note_sequence):
         for note_name in name_list:
-            note_index_map['major'][note_name] = i
-            
-    #set up the minor key indexes. c is 0 for major and a is 0 for minor, so add 3 semitones to every major index
-    note_index_map['minor'] = {name:index + 3 for name, index in note_index_map['major'].iteritems()}
+            note_index_map[note_name] = i
     
     #first we need to find the index of this note
-    note_index = note_index_map[key_type][key_name]
+    note_index = note_index_map[key_name]
     
-    #find this key's position on the circle of fifths
+    wheel_position = get_wheel_position(note_index, key_name, key_type)
+    
+    modified_notes = get_modified_notes(wheel_position)
+    
+    key_notes = get_key_notes(note_index, key_type)
+    
+    
+    #key_notes now contains the indexes of the notes that will be used in this key, with the base note at key_notes[0]
+    return key_notes
+
+def get_wheel_position(note_index, key_name, key_type):
+    
+    #the key index is different from the base note index if we're in a minor key
+    if(key_type == 'minor'):
+        key_index = (note_index + 3)%12
+    else:
+        key_index = note_index
+        
+    
+     #find this key's position on the circle of fifths
     #keys progress up and down from C using a circle of fifths. so the "base" key is at C, 
     #then we go up one perfect fifth to G, thne up one perfect fifth to D. this is equivalent to solving 
-    #"note_index congruent to perfect_fith_interval * wheel_position (mod 12)" for wheel_position
+    #"key_index congruent to perfect_fith_interval * wheel_position (mod 12)" for wheel_position
     
-    #the solution to that formula is simply "note_index * perfect_fifth_interval (mod 12)"
-    wheel_position = (note_index * 7) % 12
+    #the solution to that formula is simply "key_index * perfect_fifth_interval (mod 12)"
+    wheel_position = (key_index * 7) % 12
     
     #for the keys that involve flat notes, we will represent this with negative numbers
     if(wheel_position >= 8):
@@ -61,11 +77,11 @@ def get_key(key_name, key_type):
     elif(wheel_position >= 5 and 'flat' in key_name):
         wheel_position -= 12
         
-    #wheel_position now contains an integer from -7 to +7 representing its position on the wheel of fifths
-    
+    return wheel_position
+        
+def get_key_notes(note_index, key_type):
     #set up the initial note configuration assuming a key of c major or a minor
     if(key_type == 'major'):
-        tone_modifier = 1
         key_notes = [
             note_index, #c
             (note_index + 2)%12,#d
@@ -85,8 +101,33 @@ def get_key(key_name, key_type):
             (note_index + 8)%12,#f
             (note_index + 10)%12,#g
         ]
-    
-    #key_notes now contains the indexes of the notes that will be used in this key, with the base note at key_notes[0]
     return key_notes
+
+def get_modified_notes(wheel_position):
+    #if the wheel position is positive we will be raising notes by one half tone, 
+    #and if its negative we'll be decreasting notes by one half tone
+    if(wheel_position > 0):
+        tone_modifier = 1
+    elif(wheel_position < 0):
+        tone_modifier = -1
+    else:
+        tone_modifier = 0
+        
+    #loop from 1 to wheel_position, inclusive
+    modified_notes = set()
+    for i in xrange(1, abs(wheel_position) + 1):
+        current_note = (7 * i * tone_modifier) % 12
+        
+        #if this is a sharp key, go 2 semitones back from current_key, and increase that by one semitone
+        if(tone_modifier > 0):
+            modified_note = (current_note - 2 + tone_modifier) % 12
+        
+        #if this is a sharp key, go 6 semitones back from current_key, and increase that by one semitone
+        elif(tone_modifier < 0):
+            modified_note = (current_note - 6 + tone_modifier) % 12
+            
+        modified_notes.add(modified_note)
+        
+    return modified_notes
         
         
